88491e0487fbbca0f453dc6b872f9908e8967c3e
Milena_2004 <milenabegiyan20@gmail.com>
2020-07-14 23:44:23 +0400

    Javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+const name1 = prompt("Hello,please enter the number 4");
+console.log(name);
+const name2 = prompt("Now please enter the number 6");
+console.log(`${4 + 6}`);
+alert("Check the console :D");
